458d33a4c10cc5da23a4d56945857ea3ac38fab4
4015a15 <4015a15@granada.ece.uprm.edu>
2017-11-15 13:09:10 -0400

    Added a star shaped polygon and a commented out arrow shaped polygon.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -66,5 +66,6 @@ public class MyPanelClass extends JPanel {
                         p2.addPoint(x1 + 38, y1 + 83);
                         g.setColor(Color.WHITE);
                         g.drawPolygon(p2);
+                        
             }
 }
